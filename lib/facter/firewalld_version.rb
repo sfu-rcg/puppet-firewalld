5ac7ceadf7598fef8e24dcc1d6a3e9300656be41
asasfu <asa188@sfu.ca>
2016-08-16 10:39:29 -0700

    Fix firewalld fact for disabled firewalld users
    FirewallD would return 'FirewallD is not running' to stdout for users that didn't have it enabled.  We now match against version numbers or return nil.

--- lib/facter/firewalld_version.rb
+++ lib/facter/firewalld_version.rb
@@ -2,5 +2,12 @@
 
 Facter.add("firewalld_version") do
   confine :kernel => 'Linux'
-  setcode 'firewall-cmd --version'
+  setcode do
+    if Facter::Util::Resolution.which('firewall-cmd')
+      results = Facter::Util::Resolution.exec('firewall-cmd --version').match(/^(\d+\.\d+\.\d+)$/)
+      if results
+        results[1]
+      end
+    end
+  end
 end
